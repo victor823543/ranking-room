37141c45fef958ad8d5fbe3910c5d3239b076016
Victor <v.safsten@gmail.com>
2024-11-25 17:55:08 +0100

    Create google login functionality

--- src/components/authentication/GoogleLoginButton/GoogleLoginButton.tsx
+++ src/components/authentication/GoogleLoginButton/GoogleLoginButton.tsx
@@ -1,43 +1,45 @@
-// import { useGoogleLogin } from "@react-oauth/google";
-// import { useMutation } from "@tanstack/react-query";
-// import { useNavigate } from "react-router-dom";
-// import { useAuth } from "../../../provider/authProvider";
-// import { callAPI } from "../../../utils/apiService";
+import { useGoogleLogin } from "@react-oauth/google";
+import { useMutation } from "@tanstack/react-query";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import GoogleSvg from "../../../assets/svgs/GoogleSvg";
+import { useAuth } from "../../../provider/authProvider";
+import { callAPI } from "../../../utils/apiService";
 import CustomizableButton from "../../common/Buttons/CustomizableButton";
 import styles from "./GoogleLoginButton.module.css";
+
 const GoogleLoginButton: React.FC = () => {
-  // const { setToken } = useAuth();
-  // const navigate = useNavigate();
+  const { setToken } = useAuth();
+  const navigate = useNavigate();
 
-  // const mutation = useMutation({
-  //   mutationFn: (googleToken: string) =>
-  //     callAPI<{ token: string }>("/auth/google-login", "POST", {
-  //       token: googleToken,
-  //     }),
-  //   onSuccess: async (response) => {
-  //     setToken(response.token);
-  //   },
-  //   onError: (error) => {
-  //     console.error(error);
-  //   },
-  // });
+  const mutation = useMutation({
+    mutationFn: (googleToken: string) =>
+      callAPI<{ token: string }>("/auth/google-login", "POST", {
+        token: googleToken,
+      }),
+    onSuccess: async (response) => {
+      setToken(response.token);
+      navigate("/");
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
 
-  // const handleGoogleLoginSuccess = (response: any) => {
-  //   const googleToken = response.access_token;
-  //   mutation.mutate(googleToken);
-  // };
+  const handleGoogleLoginSuccess = (response: any) => {
+    const googleToken = response.access_token;
+    mutation.mutate(googleToken);
+  };
 
-  // const login = useGoogleLogin({
-  //   onSuccess: handleGoogleLoginSuccess,
-  //   onError: () => console.log("Login Failed"),
-  // });
+  const login = useGoogleLogin({
+    onSuccess: handleGoogleLoginSuccess,
+    onError: () => console.log("Login Failed"),
+  });
 
   return (
     <CustomizableButton
       onClick={(e) => {
-        e.preventDefault();
+        e.preventDefault(), login();
       }}
       width="full"
       variant="opaque"
