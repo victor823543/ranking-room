69012b0d54d6c8b28971eb7b387f49c4097ab178
Victor <v.safsten@gmail.com>
2024-12-05 13:43:09 +0100

    Create initial homepage header

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,9 +1,26 @@
+import LoadingPage from "../components/common/Loading/LoadingPage/LoadingPage";
+import DashboardHeader from "../components/dashboard/DashboardHeader/DashboardHeader";
+import GridLayout from "../components/layout/GridLayout/GridLayout";
 import Layout from "../components/layout/Layout/Layout";
+import { useAuth } from "../provider/authProvider";
 
 const Dashboard = () => {
+  const { user } = useAuth();
+
+  if (user === undefined) {
+    return <LoadingPage name="Dashboard" />;
+  }
+
+  if (user === null) {
+    return (
+      <Layout name="Dashboard">
+        <div></div>
+      </Layout>
+    );
+  }
   return (
     <Layout name="Dashboard">
-      <div></div>
+      <GridLayout topLeftShortWide={<DashboardHeader user={user} />} />
     </Layout>
   );
 };
